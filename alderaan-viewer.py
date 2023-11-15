66f9bfa884dbed5626a808081c5a0806b2de2a31
pentrican10 <paige.eee1014@gmail.com>
2023-11-14 22:38:10 -0800

    reorganize
    create new alderaan-viewer and organize

--- alderaan-viewer.py
+++ alderaan-viewer.py
@@ -1,7 +1,3 @@
-# add short cadence 
-# get results and clone whole repository and get to work in jupyter
-# import results class from results.py
-# launch jupyter
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import os
 import csv
@@ -14,8 +10,14 @@ import json
 import plotly.utils
 import numpy as np
 import sys
-sys.path.append('c:\\Users\\Paige\\Projects\\alderaan\\')
-import myResults
+import lightkurve as lk
+import numpy.polynomial.polynomial as poly
+import data_load
+
+#sys.path.append('c:\\Users\\Paige\\Projects\\alderaan\\')
+
+data_directory = 'c:\\Users\\Paige\\Projects\\data\\'
+
 
 app = Flask(__name__)
 app.secret_key = 'super_secret'
@@ -41,32 +43,25 @@ def logout():
 
 @app.route('/home')
 def display_table_data():
-    table_data = read_table_data()
+    """
+    Assigns each html file to their respective locations
+    Renders Index Template
+    """
+    table_data = data_load.read_table_data()
     left_content = render_template('left.html', table_data=table_data)
     right_top_content = render_template('right_top.html')
     right_bottom_content = render_template('right_bottom.html')
-    return render_template('index1.html',left_content=left_content, right_top_content=right_top_content, right_bottom_content=right_bottom_content)
+    return render_template('index.html',left_content=left_content, right_top_content=right_top_content, right_bottom_content=right_bottom_content)
 
-
-def read_table_data():
-    file_path = os.path.join(os.path.dirname(__file__), 'data', '2023-05-19_singles.csv')
-    table_data = []
-    with open(file_path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            #round table values
-            row['kep_mag'] = round(float(row['kep_mag']), 2)
-            row['Rstar'] = round(float(row['Rstar']), 2)
-            row['logrho'] = round(float(row['logrho']), 2)
-            row['Teff'] = round(float(row['Teff']))
-            row['logg'] = round(float(row['logg']), 2)
-            table_data.append(row)
-    return table_data
-
-#environment variable -- mini app
 @app.route('/star/<koi_id>')
 def display_comment_file(koi_id):
-    file_path = os.path.join('C:\\Users\\Paige\\Projects','miniflask','comment_files',f'{koi_id}_comments.txt')
+    """
+    Function to display comment file associated with KOI ID
+    args: 
+        koi_id: string, format K00000
+    """
+    path_extension = os.path.join('comment_files', f'{koi_id}_comments.txt')
+    file_path = os.path.join(data_directory, path_extension)
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
                 file_content = file.read()
@@ -74,84 +69,15 @@ def display_comment_file(koi_id):
         file_content = f'Comment file for {koi_id} not found.'
     return file_content
 
-
-# DATA 
-def read_data_from_fits(file_path):
-    with fits.open(file_path) as fits_file:
-        time = np.array(fits_file[1].data, dtype=float)
-        flux = np.array(fits_file[2].data, dtype=float)
-        df = pd.DataFrame(dict(
-            TIME=time,
-            FLUX=flux
-        ))
-    return df
-
-def fetch_data(koi_id, line_number):
-    star_id = koi_id.replace("K","S")
-    #file_name_lc = star_id + '_lc_detrended.fits'
-    #file_path_lc = os.path.join('C:\\Users\\Paige\\Projects','miniflask','kepler_lightcurves_for_paige',file_name_lc)
-    file_name_lc = star_id + '_lc_filtered.fits'
-    file_path_lc = os.path.join('C:\\Users\\Paige\\Projects','alderaan-viewer','data',file_name_lc)
-    
-    file_name_sc = star_id + '_sc_filtered.fits'
-    file_path_sc = os.path.join('C:\\Users\\Paige\\Projects','alderaan-viewer','data', file_name_sc)
-
-    combined_data = None
-    #get data and create detrended light curve
-    if os.path.isfile(file_path_lc):
-        photometry_data_lc = read_data_from_fits(file_path_lc) #descriptive names
-        transit_values, center_time_values = read_center_time_values_from_file(koi_id)
-
-        if line_number < len(transit_values):
-            center_time = center_time_values[line_number]
-            transit_number = transit_values[line_number]
-        
-            start_time = float(center_time) - 0.25
-            end_time= float(center_time) + 0.25
-
-            use_lc = (photometry_data_lc['TIME'] > start_time) & (photometry_data_lc['TIME'] < end_time)
-            lc_data = photometry_data_lc[use_lc]
-            combined_data = lc_data
-            #return filtered_data, transit_number, center_time ############
-    if os.path.isfile(file_path_sc):
-            photometry_data_sc = read_data_from_fits(file_name_sc)
-            if combined_data is not None:
-                use_sc = (photometry_data_sc['TIME'] > start_time) & (photometry_data_sc['TIME'] < end_time)
-                sc_data = photometry_data_sc[use_sc]
-                combined_data= pd.concat([combined_data, sc_data],ignore_index=True)
-            else:
-                combined_data = photometry_data_sc
-
-    return combined_data, transit_number, center_time ############
-    #else:
-    #    return None, None, None
-    
-
-
-def read_center_time_values_from_file(koi_id): #read_ct_and_transit_number_from_file(koi_id)
-    star_id = koi_id.replace("K","S")
-    file_name = star_id + '_00_quick.ttvs'
-    file_path = os.path.join('C:\\Users\\Paige\\Projects','miniflask','quick_ttvs_for_paige',file_name)
-
-    center_time_values=[] 
-    transit_value =[]
-    # Open the file for reading
-    with open(file_path, 'r') as file:
-        # Iterate through each line in the file
-        for line in file:
-            # Split the line into columns based on the delimiter
-            columns = line.strip().split('\t')
-
-            center_time_values.append(columns[1])
-            transit_value.append(columns[0])
-        
-    #print(center_time_values)
-    return transit_value, center_time_values
-    
-
 @app.route('/star/<koi_id>/save_comment', methods=['POST'])
 def save_comment(koi_id):
-    file_path = os.path.join('C:\\Users\\Paige\\Projects','miniflask','comment_files',f'{koi_id}_comments.txt')
+    """
+    function saves the comment input by user to the associated comment file
+    Saves with username, date, comment
+    Returns the function to display the updated comment file
+    """
+    path_extension = os.path.join('comment_files', f'{koi_id}_comments.txt')
+    file_path = os.path.join(data_directory, path_extension)
     comment = request.form.get('comment').strip()
     username = session.get('username')
     with open(file_path, 'a') as file:
@@ -161,10 +87,13 @@ def save_comment(koi_id):
         file.write(f"Comment: {comment}\n")
     return display_comment_file(koi_id)
 
-
 @app.route('/star/<koi_id>/edit_file', methods=['POST'])
 def save_file(koi_id):
-    file_path = os.path.join('C:\\Users\\Paige\\Projects', 'miniflask', 'comment_files', f'{koi_id}_comments.txt')
+    """
+    saves the file and displays the updated file or an error message
+    """
+    path_extension = os.path.join('comment_files', f'{koi_id}_comments.txt')
+    file_path = os.path.join(data_directory, path_extension)
     content = request.form.get('content')
     # Normalize line endings to Unix-style (\n)
     content = content.replace('\r\n', '\n')
@@ -180,28 +109,28 @@ def save_file(koi_id):
 def generate_plot_Detrended_Light_Curve(koi_id):
     star_id = koi_id.replace("K","S")
     file_name_lc = star_id + '_lc_detrended.fits'
-    file_path_lc = os.path.join('C:\\Users\\Paige\\Projects','miniflask','kepler_lightcurves_for_paige',file_name_lc)
+    file_path_lc = os.path.join(data_directory,'kepler_lightcurves_for_paige',file_name_lc)
     file_name_sc = star_id + '_sc_filtered.fits'
-    file_path_sc = os.path.join('C:\\Users\\Paige\\Projects','alderaan-viewer','data', file_name_sc)
+    file_path_sc = os.path.join(data_directory, file_name_sc)
 
-    #get data and create detrended light curve
+    ### get data and create detrended light curve
     if os.path.isfile(file_path_lc) and os.path.isfile(file_path_sc):
-        data_lc = read_data_from_fits(file_path_lc)
-        data_sc = read_data_from_fits(file_path_sc)
+        data_lc = data_load.read_data_from_fits(file_path_lc)
+        data_sc = data_load.read_data_from_fits(file_path_sc)
         fig = px.scatter(data_lc, x="TIME", y="FLUX")#, 
                     #title="Kepler Detrended Light Curve")
         fig.add_scatter(x=data_sc['TIME'], y=data_sc['FLUX'],mode='markers')
         graph1JSON= json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
         return jsonify(graph1JSON)
     
-    elif os.path.isfile(file_path_lc):
-        data_lc = read_data_from_fits(file_path_lc)
+    if os.path.isfile(file_path_lc):
+        data_lc = data_load.read_data_from_fits(file_path_lc)
         fig = px.scatter(data_lc, x="TIME", y="FLUX")
         graph1JSON= json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
         return jsonify(graph1JSON)
     
     elif os.path.isfile(file_path_sc):
-        data_sc = read_data_from_fits(file_path_sc)
+        data_sc = data_load.read_data_from_fits(file_path_sc)
         fig = px.scatter(data_sc, x="TIME", y="FLUX")
         graph1JSON= json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
         return jsonify(graph1JSON)
@@ -209,13 +138,12 @@ def generate_plot_Detrended_Light_Curve(koi_id):
     else:
         error_message = f'No data found for {koi_id}'
         return jsonify(error_message=error_message)
-        
-# light curve file: has quarter, use this to separate viewing, dashed line
-# function that generates plot 2, individual transits
-@app.route('/plot2/<koi_id>/<int:line_number>')#<int:start_time>/<int:end_time>')
-def plot2(koi_id, line_number): 
-    if (fetch_data(koi_id, line_number)):
-        photometry_data, true_transit_number, center_time = fetch_data(koi_id, line_number)
+
+''' 
+@app.route('/generate_plot_single_transit/<koi_id>/<int:line_number>')
+def generate_plot_single_transit(koi_id, line_number):
+    if (data_load.fetch_data(koi_id, line_number)):
+        photometry_data, true_transit_number, center_time = data_load.fetch_data(koi_id, line_number)
 
         fig = px.scatter(photometry_data, x="TIME", y="FLUX") #only want to plot data within selected time window
         #fig.update_traces(marker=dict(
@@ -241,23 +169,19 @@ def plot2(koi_id, line_number):
         error2 = f'No data found for {koi_id}'
         return jsonify(error2=error2)
 
-@app.route('/get_transit_numbers/<koi_id>')
-def get_transit_numbers(koi_id):
-    transit_values, center_time_values = read_center_time_values_from_file(koi_id)
-    return jsonify(transit_values)
+@app.route('/generate_plot_folded_light_curve/<koi_id>')
+def generate_plot_folded_light_curve(koi_id):
+    x = np.linspace(1,4,1000)
+    y = np.linspace(3,8,1000)
+    fig = px.scatter(x,y)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
+    return jsonify(graphJSON)
+'''
+
+#@app.route('/generate_plot/<koi_id>')
 
 
-def o_minus_c_plot():
-
-    return
-
-def folded_transit_plot():
-
-    return
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# to plotly function to take data and make interactive 
